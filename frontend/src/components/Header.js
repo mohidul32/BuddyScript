96acb49203415d679cdc7420d7f2bc734629c52b
mohidul <exit.mohidulislam@gmail.com>
2025-11-24 23:44:32 +0600

    Removed friend request and feed page navigation links from navbar as they are not fully functional yet

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -75,16 +75,12 @@ const Header = () => {
           {/* Navigation Icons */}
           <ul className="navbar-nav mb-2 mb-lg-0 _header_nav_list ms-auto _mar_r8">
             <li className="nav-item _header_nav_item">
-              <Link className="nav-link _header_nav_link_active" to="/feed">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="21" fill="none" viewBox="0 0 18 21">
-                  <path className="_home_active" stroke="#000" strokeWidth="1.5" strokeOpacity=".6" d="M1 9.924c0-1.552 0-2.328.314-3.01.313-.682.902-1.187 2.08-2.196l1.143-.98C6.667 1.913 7.732 1 9 1c1.268 0 2.333.913 4.463 2.738l1.142.98c1.179 1.01 1.768 1.514 2.081 2.196.314.682.314 1.458.314 3.01v4.846c0 2.155 0 3.233-.67 3.902-.669.67-1.746.67-3.901.67H5.57c-2.155 0-3.232 0-3.902-.67C1 18.002 1 16.925 1 14.77V9.924z" />
-                </svg>
-              </Link>
+              
             </li>
 
             <li className="nav-item _header_nav_item">
               <Link className="nav-link _header_nav_link" to="/friend-requests">
-                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="20" fill="none" viewBox="0 0 26 20">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1" height="1" fill="none" viewBox="0 0 26 20">
                   <path fill="#000" fillOpacity=".6" fillRule="evenodd" d="M12.79 12.15h.429c2.268.015 7.45.243 7.45 3.732 0 3.466-5.002 3.692-7.415 3.707h-.894c-2.268-.015-7.452-.243-7.452-3.727 0-3.47 5.184-3.697 7.452-3.711l.297-.001h.132z" clipRule="evenodd" />
                 </svg>
                 {friendRequests.length > 0 && (
