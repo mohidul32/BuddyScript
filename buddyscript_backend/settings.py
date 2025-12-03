3e02114821964352ef3da3e128c121aeca91e56b
mohidul <exit.mohidulislam@gmail.com>
2025-12-04 01:10:20 +0600

    deploy frontend on vercel

--- buddyscript_backend/settings.py
+++ buddyscript_backend/settings.py
@@ -174,9 +174,13 @@ SIMPLE_JWT = {
 }
 
 # CORS Settings
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+# ]
+
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "https://buddy-script-three.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
