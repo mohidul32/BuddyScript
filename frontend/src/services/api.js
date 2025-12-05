c15ab7d2ec580ac7598fdfb81c871590d93f0ff5
mohidul <exit.mohidulislam@gmail.com>
2025-12-06 00:46:49 +0600

    backend localhost url added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://web-production-79de8.up.railway.app/api';
+// const API_URL = 'http://127.0.0.1:8000/api';
 
 const api = axios.create({
   baseURL: API_URL,
